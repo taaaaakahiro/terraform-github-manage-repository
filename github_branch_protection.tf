e64ab4d5bde7006867443aab7a08b0f48153390f
takahiro <takapi854@gmail.com>
2024-07-17 12:30:54 +0900

    fix tf files

--- github_branch_protection.tf
+++ github_branch_protection.tf
@@ -3,7 +3,8 @@
 resource "github_branch_protection" "terraform-github-template-main" {
   repository_id = github_repository.terraform-github-template.node_id
 
-  pattern          = "main"
-  enforce_admins   = true
-  allows_deletions = false
+  pattern                = "main"
+  enforce_admins         = true
+  allows_deletions       = false
+  require_signed_commits = true
 }
